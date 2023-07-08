1149cc8e06fcf68de902ebb6b403e0814d1a67f9
Rishabh Bassi <rbassi@linkedin.com>
2023-07-08 16:55:18 -0700

    Education

--- src/pages/education/EducationComponent.js
+++ src/pages/education/EducationComponent.js
@@ -23,15 +23,27 @@ function Education(props) {
                 Education
               </h1>
               <h3 className="heading-sub-text" style={{ color: theme.text }}>
-                Basic Qualification and Certifications
+                Degrees and Certifications
               </h3>
               <p
                 className="experience-header-detail-text subTitle"
                 style={{ color: theme.secondaryText }}
               >
-                I actively participate in hackathons and other tech-related
-                activities. Below are some of my major certifications.
+                Welcome to my personal website, which highlights my eduaction ,research and development projects as well as my personal hobbies.
               </p>
+              <p
+                className="experience-header-detail-text subTitle"
+                style={{ color: theme.secondaryText }}
+              >
+                In Computer Science Domain, My broad area of interest lies in the fields of ML, NLP and their far reaching applications in Healthcare. I'm interested in developing both front-end and back-end code in order to construct full-stack user apps.
+              </p>
+              <p
+                className="experience-header-detail-text subTitle"
+                style={{ color: theme.secondaryText }}
+              >
+                I love to solve challenges and always eager to learn something new be it online courses or life necessities like cooking. I actively participate in hackathons and other tech-related activities.
+              </p>
+
             </div>
           </div>
         </Fade>
